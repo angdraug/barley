268a351f6015cceeeca73286c727679542c45e04
Dmitry Borodaenko <angdraug@debian.org>
2020-12-04 17:20:59 -0800

    Make CryptPad work with data in /var/lib
    See upstream pull request:
    https://github.com/xwiki-labs/cryptpad/pull/647

--- packer/cryptpad.sh
+++ packer/cryptpad.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone --depth 1 -b 3.24.0 https://github.com/xwiki-labs/cryptpad.git cryptpad
+git clone --depth 1 -b pass-archivepath-v3.24.0 https://github.com/angdraug/cryptpad.git cryptpad
 cd cryptpad
 rm -rf .git
 npm install
