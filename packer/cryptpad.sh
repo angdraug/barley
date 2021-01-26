4387a911e07134dfc175e7843781feb9235dec21
Dmitry Borodaenko <angdraug@debian.org>
2021-01-26 15:33:50 -0800

    Update CryptPad to 4.0

--- packer/cryptpad.sh
+++ packer/cryptpad.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone --depth 1 -b pass-archivepath-v3.24.0 https://github.com/angdraug/cryptpad.git cryptpad
+git clone --depth 1 -b pass-archivepath-v4.0 https://github.com/angdraug/cryptpad.git cryptpad
 cd cryptpad
 rm -rf .git
 npm install
